64c5eac380b48cf612b3430864d4f3df38c78a83
Zineb Atlassi <atlassizineb@gmail.com>
2023-01-11 18:24:39 +0100

    Aggiunta bottone

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 /*
-1. creo di div cella con numerazione da 1 a 100
-2. al click dell'utente sulla cella si colora di azzurro grazie alla classe:
+1. cliccando sul bottone generate compare la griglia
+2. creo di div cella con numerazione da 1 a 100
+3. al click dell'utente sulla cella si colora di azzurro grazie alla classe:
     - appare in console il numero
 */
 
+// seleziono la griglia
 const gridContainer = document.getElementById('grid-container');
 console.log('Ho selezionato l\'elemento', gridContainer);
+
+//seleziono il bottone
+const generateButton = document.getElementById('generate-button');
+console.log('Ho selezionato l\'elemento', generateButton);
+
+// cliccandocompare la griglia
+const main = document.getElementById('main');
+generateButton.addEventListener('click',
+    function() {
+        main.classList.add('flex');
+    }
+)
+
 
 for(let i = 1 ; i <= 100 ; i++){
     console.log('I numeri all\'interno delle celle sono', i);
